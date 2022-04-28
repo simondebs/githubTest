7e66d680dc0b54bff2accd63c177c719af41c3bc
simondebs <simon.debs19@gmail.com>
2022-04-28 15:45:03 +0300

    Update hello.cpp

--- hello/hello/hello.cpp
+++ hello/hello/hello.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World: my name is harik !\n";
+    std::cout << "Hello World: my name is harik!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
